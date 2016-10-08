6d62111aa44bf7299406aa3876c8a56d414fd953
Sydney Kurtz <sydney.kurtz@yahoo.com>
2016-10-08 16:41:00 -0500

    udpdate

--- liri.js
+++ liri.js
@@ -13,8 +13,6 @@ tweetTweet();
 function tweetTweet() {
     // Establish Twitter object
 
-
-
     //var client = new Twitter(key.twitterKeys);
     var params = { screen_name: 'whitegrlproblem' };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -33,14 +31,26 @@ function tweetTweet() {
 function playSpotify() {
     var spotify = require('spotify');
 
-    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    spotify.search({ type: 'track', query: 'songName' }, function(err, data) {
         if (err) {
-            console.log('Error occured: ' + err);
-            return;
+            console.log('Error occurred: ' + err);
+            return; // debugger
+            
         }
-        // do something with data
-    });
+        // do something with data'
+     var songs = data.tracks.items;
+
+	    for(var i = 0; i < songs.length; i++){
+	    	console.log(i);
+	    	console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
+	    	console.log('song name: ' + songs[i].name);
+	    	console.log('preview song: ' + songs[i].preview_url);
+	    	console.log('album: ' + songs[i].album.name);
+	    	console.log('-----------------------------------');
+	    }
+	});
 }
+
 
 function doIt() {
     switch (datThang) {
@@ -48,7 +58,7 @@ function doIt() {
             tweetTweet();
             break;
         case 'spotify-this-song':
-            spotifyThisSong();
+            playSpotify();
             break;
         case 'do-what-it-says':
             doWhatItSays();
