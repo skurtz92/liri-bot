3041367cda7a076c8040766bf033fb94aec54012
Sydney Kurtz <sydney.kurtz@yahoo.com>
2016-10-07 22:30:34 -0500

    update

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'FIokZLBfuqJ7Lu0XW9rhJJ6yc',
+  consumer_secret: 'XPxgfikKdYV7IspMK57Px40pCFvs3fELfrcFyGX85zhv7VjP7w',
+  access_token_key: '780218312554782720-KqqZgnwXmQ1eE5EQWMy7PeoCuRYT6yw',
+  access_token_secret: 'M43WdoXr49B6hxwHBZA8xQ5IhFJuvCsrxsGG9sCrEc5UK',
 }
